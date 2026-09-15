28c7046b1fa4ca2fd648f98013ff83dd713a40db
Robert <srellister@gmail.com>
2026-09-14 18:20:47 -0600

    Initial setup

---
+++ lessons/Day-02-Demonstration-Starter/bs-demo-starter-02/Day02Demo/Program.cs
@@ -0,0 +1,23 @@
+using Day02Demo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
