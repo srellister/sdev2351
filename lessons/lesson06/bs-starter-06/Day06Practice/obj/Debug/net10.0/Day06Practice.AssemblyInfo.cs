2a9e63dc07f26e7495c1284ba793b243bb436e67
relliott13nait <relliott13@nait.ca>
2026-09-17 19:43:08 -0600

    lesson 6 work

--- lessons/lesson06/bs-starter-06/Day06Practice/obj/Debug/net10.0/Day06Practice.AssemblyInfo.cs
+++ lessons/lesson06/bs-starter-06/Day06Practice/obj/Debug/net10.0/Day06Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day06Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c6a65d9f5064c8aaa8d8a10cbc227f25ce64f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day06Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day06Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
