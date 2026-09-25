cb1e1e86dfc7eb822c1402c50cac325c950152cd
relliott13nait <relliott13@nait.ca>
2026-09-24 19:39:28 -0600

    lab 1 added

--- lessons/lesson08/bs-starter-08/Day08Practice/obj/Debug/net10.0/Day08Practice.AssemblyInfo.cs
+++ lessons/lesson08/bs-starter-08/Day08Practice/obj/Debug/net10.0/Day08Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day08Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6566df3d5c7d4458d1bb441b2287524b80d93f5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4340b7ea0bbbb0e7f216b1e03e00d7b6b800ad7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day08Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day08Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
